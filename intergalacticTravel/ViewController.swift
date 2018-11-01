e689ac0434e840c7c7f13a98f962ad6de2f0ae60
Kyle Chung <kchung1068@jh-mba158-04.local>
2018-11-01 14:49:19 -0500

    Initial Commit

---
+++ intergalacticTravel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  intergalacticTravel
+//
+//  Created by Kyle Chung on 11/1/18.
+//  Copyright © 2018 Kyle Chung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
